d9fc45d68cacc446eb3de5fc36d1924198a9f3b8
Alexandre <alexandre.hulsken@gmail.com>
2018-10-09 09:47:03 +0200

    refacto(App): delete useless components

--- front-angular/src/app/app.module.ts
+++ front-angular/src/app/app.module.ts
@@ -13,16 +13,13 @@ import { AppRoutingModule } from "./shared/core/app-routing.module";
 import { AppComponent } from './app.component';
 
 import { NavBarComponent } from './feat/nav-bar/nav-bar.component';
-import { TestsListComponent } from './feat/tests-list/tests-list.component';
 import { CreateItemComponent } from './feat/create-item/create-item.component';
 import { AccountComponent } from './feat/account/account.component';
-import { TestsListItemComponent } from './feat/tests-list/tests-list-item/tests-list-item.component';
 
 import { TestLevelService } from "./shared/service/test-level.service";
 
 import { TestByLevelResolver } from "./shared/resolver/test-by-level.resolver";
 import { AllTestLevelResolver } from "./shared/resolver/all-test-level.resolver";
-import { AnalyzeComponent } from './feat/analyze/analyze.component';
 import { LoadingComponent } from './shared/component/loading/loading.component';
 import { ImportFileItemComponent } from './feat/create-item/import-file-item/import-file-item.component';
 import { CreateOnlineItemComponent } from './feat/create-item/create-online-item/create-online-item.component';
@@ -35,11 +32,8 @@ import { CreateReadingItemComponent } from './feat/create-item/create-online-ite
   declarations: [
     AppComponent,
     NavBarComponent,
-    TestsListComponent,
     CreateItemComponent,
     AccountComponent,
-    TestsListItemComponent,
-    AnalyzeComponent,
     LoadingComponent,
     ImportFileItemComponent,
     CreateOnlineItemComponent,
--- front-angular/src/app/shared/core/app-routing.module.ts
+++ front-angular/src/app/shared/core/app-routing.module.ts
@@ -1,12 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { TestsListComponent } from '../../feat/tests-list/tests-list.component';
-import { AllTestLevelResolver } from '../resolver/all-test-level.resolver';
 import { CreateItemComponent } from '../../feat/create-item/create-item.component';
 import { AccountComponent } from "../../feat/account/account.component";
-import { TestComponent } from "../../feat/test/test.component";
-import { TestByLevelResolver } from "../resolver/test-by-level.resolver";
-import { AnalyzeComponent } from "../../feat/analyze/analyze.component";
 
 export const appRoutes: Routes = [
   {
@@ -20,29 +15,6 @@ export const appRoutes: Routes = [
       {
         path: 'account',
         component: AccountComponent,
-      },
-      {
-        path: 'results',
-        component: AnalyzeComponent
-      },
-      {
-        path: 'test',
-        children: [
-          {
-            path: '',
-            component: TestsListComponent,
-            resolve: {
-              levelList: AllTestLevelResolver
-            }
-          },
-          {
-            path: ':level',
-            component: TestComponent,
-            resolve: {
-              itemList: TestByLevelResolver
-            }
-          }
-        ]
       }
     ]
   },

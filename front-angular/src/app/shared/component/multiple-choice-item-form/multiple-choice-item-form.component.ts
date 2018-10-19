838e8bba82f0e384f61790b550521ff947f7d7e3
Alexandre <alexandre.hulsken@gmail.com>
2018-10-19 22:52:11 +0200

    feat(forms): deep copy lists for forms

--- front-angular/src/app/shared/component/multiple-choice-item-form/multiple-choice-item-form.component.ts
+++ front-angular/src/app/shared/component/multiple-choice-item-form/multiple-choice-item-form.component.ts
@@ -10,7 +10,11 @@ import {
   FormGroup,
   Validators
 } from "@angular/forms";
-import { MultipleChoiceItem } from "../../model/items/multiple-choice-item";
+
+import {
+  buildEmptyMultipleChoiceItem,
+  MultipleChoiceItem
+} from "../../model/items/multiple-choice-item";
 import { Tools } from "../../core/tools.module";
 
 
@@ -40,7 +44,7 @@ export class MultipleChoiceItemFormComponent implements OnChanges {
       description: [this.multipleChoiceItemToCreate.description, Validators.required],
       level: [this.multipleChoiceItemToCreate.level, Validators.required],
       correctAnswer: [this.multipleChoiceItemToCreate.correctAnswer, Validators.required],
-      content: [this.multipleChoiceItemToCreate.content]
+      content: [Tools.deepCopyArray(this.multipleChoiceItemToCreate.content)]
     });
   }
 
@@ -49,7 +53,9 @@ export class MultipleChoiceItemFormComponent implements OnChanges {
       const multipleChoiceItemCreated: MultipleChoiceItem = Tools.clone(this.multipleChoiceItemToCreate);
       Object.assign(multipleChoiceItemCreated, this.multipleChoiceItemForm.value);
 
+      this.multipleChoiceItemToCreate = buildEmptyMultipleChoiceItem();
       Tools.resetForm(this.multipleChoiceItemForm, this.multipleChoiceItemToCreate);
+      this.multipleChoiceItemForm.value.content = Tools.deepCopyArray(this.multipleChoiceItemToCreate.content);
       this.onSubmit.emit(multipleChoiceItemCreated);
     } else {
       Tools.markedForm(this.multipleChoiceItemForm);
@@ -58,6 +64,7 @@ export class MultipleChoiceItemFormComponent implements OnChanges {
 
   public cancel(): void {
     Tools.resetForm(this.multipleChoiceItemForm, this.multipleChoiceItemToCreate);
+    this.multipleChoiceItemForm.value.content = Tools.deepCopyArray(this.multipleChoiceItemToCreate.content);
     this.onCancel.emit();
   }
 
